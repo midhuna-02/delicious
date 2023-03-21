85cf072f98f1d52e979606b1f50409b51b5366a9
midhuna-02 <midhunamuthungali@gmail.com>
2023-03-22 01:36:45 +0530

    search bar added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Category from './components/Category'
 
 import Pages from './components/pages/Pages'
 import {BrowserRouter} from 'react-router-dom'
+import Search from './components/Search'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Search/>
       <Category/>
       <Pages/>
       </BrowserRouter>
